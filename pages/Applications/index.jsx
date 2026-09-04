6e13d0d606c425f151b0ff17539e602fbce7faf8
Joao Vicente <joao.diogo.vicente@gmail.com>
2026-09-04 17:28:44 +0100

    feat: customize table and side panel descriptors for the applications page

--- pages/Applications/index.jsx
+++ pages/Applications/index.jsx
@@ -2,5 +2,23 @@ import React from 'react';
 import DataProductTabular from '../../../components/DataProductTabular';
 
 export default function Page() {
-    return <DataProductTabular title="Applications" tierFilter={"application"} />;
+    const tableDescriptor = [
+        { columnName: "Domain", odpsDescriptor: "domain", textMapper: (val, ctx) => ctx.formatDomain(val) },
+        { columnName: "Application", odpsDescriptor: "_customProperty(\"dataProductBusinessName\")", sidePanelLink: true },
+        { columnName: "Stage", odpsDescriptor: "_highestEnv", displayFormat: "chip" }
+    ];
+
+    const sidePanelDescriptor = [
+        { name: "ID", odpsDescriptor: "id" },
+        { name: "DOMAIN TECHNICAL NAME", odpsDescriptor: "domain" },
+        { name: "DATA SOURCE TECHNICAL NAME", odpsDescriptor: "name" },
+        { name: "TECHNOLOGY", odpsDescriptor: "_customProperty(\"technology\")" }
+    ];
+
+    return <DataProductTabular
+        title="Applications"
+        tierFilter={"application"}
+        tableDescriptor={tableDescriptor}
+        sidePanelDescriptor={sidePanelDescriptor}
+    />;
 }
